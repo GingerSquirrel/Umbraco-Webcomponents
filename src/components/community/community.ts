08fe26d7769aca7223ae2c25f3b4fb31a175bf08
Harriet Lawrie <chocolatey_squirrel@hotmail.com>
2025-06-29 14:32:05 +0100

    Change for case sensitivity

--- src/components/community/community.ts
+++ src/components/community/community.ts
@@ -2,9 +2,9 @@ import styles from './community.scss?inline';
 import template from './community.html?raw';
 import { createComponent } from '../CreateComponent';
 
-import icon1 from '../../global/images/Icon1.svg'; 
-import icon2 from '../../global/images/Icon2.svg'; 
-import icon3 from '../../global/images/Icon3.svg'; 
+import icon1 from '../../global/images/icon1.svg'; 
+import icon2 from '../../global/images/icon2.svg'; 
+import icon3 from '../../global/images/icon3.svg'; 
 
 var html = template.replace('{{Icon1}}', icon1)
   .replace('{{Icon2}}', icon2)
