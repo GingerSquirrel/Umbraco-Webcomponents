5789446073bdc88c77ef9518f914f16dda8d1af9
Harriet Lawrie <chocolatey_squirrel@hotmail.com>
2025-07-07 23:00:46 +0100

    Finished achievements frontend

--- frontend/src/components/achievements/achievements.ts
+++ frontend/src/components/achievements/achievements.ts
@@ -2,10 +2,10 @@ import styles from './achievements.scss?inline';
 import template from './achievements.html?raw';
 import { createComponent } from '../CreateComponent';
 
-import achievement1 from '../../global/images/logo1.svg'; 
-import achievement2 from '../../global/images/logo2.svg'; 
-import achievement3 from '../../global/images/logo3.svg'; 
-import achievement4 from '../../global/images/logo4.svg'; 
+import achievement1 from '../../global/images/icon-green1.svg'; 
+import achievement2 from '../../global/images/icon-green2.svg'; 
+import achievement3 from '../../global/images/icon-green3.svg'; 
+import achievement4 from '../../global/images/icon-green4.svg'; 
 
 var html = template.replace('{{achievement1}}', achievement1)
   .replace('{{achievement2}}', achievement2)
